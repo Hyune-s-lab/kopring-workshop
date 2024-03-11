761d750b1a02cc51f874bb8d96bf1bfa4894b050
Hyune-c <choi8608@gmail.com>
2024-03-11 22:19:36 +0900

    [web:exception] step 1. RestControllerAdvice - 최상위 예외 처리

--- web/exception/README.md
+++ web/exception/README.md
@@ -11,3 +11,8 @@
     - `RestControllerAdvice` 는 하나의 request 에서 에외가 발생하는 경우 global handling 을 해줍니다.
 - `@Rest~~` annotation 을 사용한다면 annotation 기반의 응답 객체 조작을 추천 합니다.
     - `ResponseEntity` 를 직접 조작하는 방법도 있습니다.
+
+### `RestControllerAdvice` 에서 handling 되지 못한 exception 은?
+
+- default 로직에서 잡히긴 하지만 권장하지 않습니다.
+- `@ExceptionHandler(Exception::class)` 선언으로 최상위 예외 처리를 권장 합니다.
--- web/exception/src/main/kotlin/com/example/kopringworkshop/exception/support/ApiControllerAdvice.kt
+++ web/exception/src/main/kotlin/com/example/kopringworkshop/exception/support/ApiControllerAdvice.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -32,11 +33,31 @@ class ApiControllerAdvice {
         }
     }
 
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ErrorResponse {
+        // also 의 정석적인 it 사용
+        return e.also { errorLogging(it) }
+            .toResponse()
+
+        // 가독성을 위해 also 를 뒤에 위치
+        //        return e.toResponse().also {
+        //            errorLogging(e)
+        //        }
+    }
+
     /**
      * 표준 예외 응답과 별개로 also 절을 통한 호출로 로깅 처리
      */
     private fun errorLogging(e: Exception) {
         log.error("handle ${e.javaClass.simpleName}", e)
+    }
+
+    /**
+     * 확장 함수를 통해 간결화할 수도 있습니다.
+     */
+    private fun Exception.toResponse(): ErrorResponse {
+        return ErrorResponse(code = this.javaClass.simpleName, message = this.message)
     }
 
     /**
